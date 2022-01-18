23ddbae609821256136aaf08ba8655cd64df9ca9
AlexanderKorn1 <92260267+AlexanderKorn1@users.noreply.github.com>
2022-01-18 11:29:15 +0100

    Add files via upload

--- wgan_mus.py
+++ wgan_mus.py
@@ -18,8 +18,10 @@ import torch
 
 import matplotlib.pyplot as plt
 
+# make directory for saving the generated images
 os.makedirs("images", exist_ok=True)
 
+# parse user arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=10, help="size of the batches")
@@ -34,51 +36,54 @@ parser.add_argument("--sample_interval", type=int, default=5, help="interval bet
 opt = parser.parse_args()
 print(opt)
 
+# define image shape
 img_shape = (opt.channels, opt.img_size, opt.img_size)
 
+# check for nvidia gpu, true if cuda driver is available
 cuda = True if torch.cuda.is_available() else False
 
-
+# class definition - Generator
 class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
+    def __init__(self):                                             
+        super(Generator, self).__init__()                           # constructor of Generator
 
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
+        def block(in_feat, out_feat, normalize=True):               # define implement layers of Generator
+            layers = [nn.Linear(in_feat, out_feat)]                 # set linear layer
             if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))        # append normalize first layer
+            layers.append(nn.LeakyReLU(0.2, inplace=True))          # append LeakyReLu layers
+            return layers                                           # return layers
 
-        self.model = nn.Sequential(
-            *block(opt.latent_dim, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
+        self.model = nn.Sequential(                                 # define Generator model
+            *block(opt.latent_dim, 128, normalize=False),           # define convolutions
+            *block(128, 256),                                       
+            *block(256, 512),                                       
             *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
-            nn.Tanh()
+            nn.Linear(1024, int(np.prod(img_shape))),               # applies linear transformation to incoming data
+            nn.Tanh()                                               # tangens hyperbolicus
         )
 
-    def forward(self, z):
-        img = self.model(z)
-        img = img.view(img.shape[0], *img_shape)
+    def forward(self, z):                                           # define help method
+        img = self.model(z)                                         # prepare image
+        img = img.view(img.shape[0], *img_shape)                    # prepare image shape
         return img
 
 
+# class definition - Discriminator
 class Discriminator(nn.Module):
     def __init__(self):
-        super(Discriminator, self).__init__()
+        super(Discriminator, self).__init__()                       # constructor of Discriminator
 
-        self.model = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 512),
+        self.model = nn.Sequential(                                 # define Discriminator model
+            nn.Linear(int(np.prod(img_shape)), 512),                # define convolution layers
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 256),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(256, 1),
         )
 
-    def forward(self, img):
-        img_flat = img.view(img.shape[0], -1)
+    def forward(self, img):                                         # define help method
+        img_flat = img.view(img.shape[0], -1)                       
         validity = self.model(img_flat)
         return validity
 
@@ -87,47 +92,46 @@ class Discriminator(nn.Module):
 generator = Generator()
 discriminator = Discriminator()
 
+# if cuda is available, use gpu
 if cuda:
     generator.cuda()
     discriminator.cuda()
 
 # Configure data loader
-#os.makedirs("../Test1/kitchen", exist_ok=True)
-
-#dataset = torchvision.datasets.ImageFolder("../Test1")
-transform = transforms.Compose([transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])
-
-dataset = datasets.ImageFolder('pics', transform=transform)
-
-dataloader = torch.utils.data.DataLoader(
+transform = transforms.Compose([transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])])    # define transformation
+dataset = datasets.ImageFolder('pics', transform=transform)         # path to input dataset and transform
+dataloader = torch.utils.data.DataLoader(                           # load dataset
     dataset,
     batch_size=opt.batch_size,
     shuffle=True,
 )
 
 # Optimizers
-optimizer_G = torch.optim.RMSprop(generator.parameters(), lr=opt.lr)
-optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=opt.lr)
+optimizer_G = torch.optim.RMSprop(generator.parameters(), lr=opt.lr)            # optimizer for Generator
+optimizer_D = torch.optim.RMSprop(discriminator.parameters(), lr=opt.lr)        # optimizer for Discriminator
 
+# prepare Torch Tensor or Cuda Tensor
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
 
 # ----------
 #  Training
 # ----------
 
 batches_done = 0
-for epoch in range(opt.n_epochs):
+for epoch in range(opt.n_epochs):                                   # iterating through the defined epochs
 
-    for i, (imgs, _) in enumerate(dataloader):
+    for i, (imgs, _) in enumerate(dataloader):                      # iterating through dataset
 
         # Configure input
-        real_imgs = Variable(imgs.type(Tensor))
+        real_imgs = Variable(imgs.type(Tensor))                     # prepare input real image
 
         # ---------------------
         #  Train Discriminator
         # ---------------------
 
-        optimizer_D.zero_grad()
+        # set all gradients of Tensors to zero
+        optimizer_D.zero_grad()                                     
 
         # Sample noise as generator input
         z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
@@ -135,18 +139,13 @@ for epoch in range(opt.n_epochs):
         # Generate a batch of images
         fake_imgs = generator(z).detach()
 
-
-        #print("Shape real: %d, Shape fake: %d" % (real_imgs.shape, fake_imgs.shape))
-        #print("Shape real:")
-        #print(real_imgs.shape)
-        #print("Shape fake:")
-        #print(fake_imgs.shape)
-
-
         # Adversarial loss
         loss_D = -torch.mean(discriminator(real_imgs)) + torch.mean(discriminator(fake_imgs))
 
+        # compute gradient
         loss_D.backward()
+
+        # perform optimizing step
         optimizer_D.step()
 
         # Clip weights of discriminator
@@ -160,21 +159,28 @@ for epoch in range(opt.n_epochs):
             #  Train Generator
             # -----------------
 
+            # set all gradients of Tensors to zero
             optimizer_G.zero_grad()
 
             # Generate a batch of images
             gen_imgs = generator(z)
+
             # Adversarial loss
             loss_G = -torch.mean(discriminator(gen_imgs))
 
+            # compute gradient
             loss_G.backward()
+
+            # perform optimizing step
             optimizer_G.step()
 
             print(
-                "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-                % (epoch, opt.n_epochs, batches_done % len(dataloader), len(dataloader), loss_D.item(), loss_G.item())
+                "[Epoch %d/%d] [D loss: %f] [G loss: %f]"
+                % (epoch, opt.n_epochs, loss_D.item(), loss_G.item())
             )
 
+        # save generated image
         if batches_done % opt.sample_interval == 0:
             save_image(gen_imgs.data[:1], "images/%d.png" % batches_done, nrow=1, normalize=True)
+        # count done batches up
         batches_done += 1
